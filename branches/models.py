41029ad4de1293e863ab1f3b542e5a803d7246c7
Ali Yawari <ali.yawari1999.12.20@gmail.com>
2023-05-22 11:23:35 -0700

    model for branch created

--- branches/models.py
+++ branches/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.text import slugify
+import uuid
+class Branch(models.Model):
+    STATUS = (
+        ("فعال", "فعال"),
+        ("غیر فعال", "غیر فعال"),
+    )
 
-# Create your models here.
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, editable=False)
+    publish_date = models.DateField()
+    status = models.CharField(choices=STATUS, default="فعال", max_length=20)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(str(uuid.uuid4()))
+        super(Branch, self).save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
